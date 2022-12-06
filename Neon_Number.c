666589c223fb598c2cc480cfe38c313a71ed360e
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2022-12-06 10:31:59 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a==1 || a==9 || a==0)     {         printf("Neon Number");     }     else     printf("Not Neon Number"); }

---
+++ Neon_Number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==1 || a==9 || a==0)
+    {
+        printf("Neon Number");
+    }
+    else
+    printf("Not Neon Number");
+}
