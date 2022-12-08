b5fa828aff6d368087f580de2bb393820eeeb8f8
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2022-12-08 23:17:03 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);    // printf("%d",n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j )             {                 printf("0");             }             else             printf("x");         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,20 +3,20 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
+   // printf("%d",n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            if(i==j || i==n || j==1)
+            if(i==j )
             {
-                printf("*");
+                printf("0");
             }
             else
-            {
-                printf(" ");
-            }
+            printf("x");
         }
         printf("
 ");
     }
+    
 }
