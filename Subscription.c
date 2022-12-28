6937e484373c302d52f2742744363b886d073020
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2022-12-28 22:59:51 +0530

    #include<stdio.h> int main() {     int a,n;     scanf("%d",&a);     while(a--)     {    scanf("%d",&n);    if(n>30)    {        printf("YES ");    }    else    printf("NO ");     } }

---
+++ Subscription.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    scanf("%d",&a);
+    while(a--)
+    {
+   scanf("%d",&n);
+   if(n>30)
+   {
+       printf("YES
+");
+   }
+   else
+   printf("NO
+");
+    }
+}
