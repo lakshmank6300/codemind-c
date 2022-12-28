1ecf830d8348e50ec808c2cda9a5c7b0af4abaa4
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2022-12-28 21:53:23 +0530

    #include<stdio.h> int main() {     int a,n;     scanf("%d",&n);     while(n--)     {     scanf("%d",&a);     if(a<3)     printf("LIGHT ");     else if(a>=3 && a<7)     printf("MODERATE ");     else if(a>=7)     printf("HEAVY ");     } }

---
+++ Rain_gauge.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    scanf("%d",&n);
+    while(n--)
+    {
+    scanf("%d",&a);
+    if(a<3)
+    printf("LIGHT
+");
+    else if(a>=3 && a<7)
+    printf("MODERATE
+");
+    else if(a>=7)
+    printf("HEAVY
+");
+    }
+}
