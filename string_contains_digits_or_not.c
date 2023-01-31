183d8eb0f8d7ba647d0e84fa3a5bc0e97ae0828a
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-02-01 00:35:30 +0530

    #include<stdio.h> #include<string.h> int main() {
        int t,n,j,i,c;
        scanf("%d",&t);
        while(t--)
        {
         c=0;
        char s[100];
        scanf("%s",s);
        n=strlen(s);
        for(i=0;i<n;i++)
        {
            if(s[i]>=47 && s[i]<=58)
            {
                c++;
            }
        }
        if(c==0)
        printf("No
    ");
        else
        printf("Yes
    ");
        }
    }

---
+++ string_contains_digits_or_not.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    
+    int t,n,j,i,c;
+    scanf("%d",&t);
+    while(t--)
+    {
+     c=0;
+    char s[100];
+    scanf("%s",s);
+    n=strlen(s);
+    for(i=0;i<n;i++)
+    {
+        if(s[i]>=47 && s[i]<=58)
+        {
+            c++;
+        }
+    }
+    if(c==0)
+    printf("No
+");
+    else
+    printf("Yes
+");
+    }
+}
