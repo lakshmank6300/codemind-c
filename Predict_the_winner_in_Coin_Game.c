9753ad823cf9a8b360829ce896c7e36c4b9020f0
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-05-02 13:44:15 +0530

    #include<stdio.h> int main(){     int n,m;     scanf("%d%d",&n,&m);     if(m<=1|| n<=1){         printf("Player 2");     }     else{         if(m%2==0 || n%2==0){             printf("Player 1");         }         else         printf("Player 2");     } }

--- Predict_the_winner_in_Coin_Game.c
+++ Predict_the_winner_in_Coin_Game.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main(){
-    int m,n;
-    scanf("%d%d",&m,&n);
-    if(m<=1 && n<=1){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    if(m<=1|| n<=1){
         printf("Player 2");
     }
     else{
-        if(m%2==0 || n%2==0)
-        printf("Player 1");
+        if(m%2==0 || n%2==0){
+            printf("Player 1");
+        }
         else
         printf("Player 2");
     }
